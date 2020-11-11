e284f082643156164463fb3b3b9023bfb2e42116
Prabhu Marappan <prabhum.794@gmail.com>
2020-11-11 22:38:11 +0530

    Added code for login

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
+const db = require("./db/db");
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-    res.send('Setup done!');
+app.use(bodyParser.json());
+
+app.post('/', (req, res) => {
+    email = req.body.email;
+    password = req.body.password;
+    db.userExists(email, password)
+        .then(function(value) {
+            // create access token -> valid for 5 mins
+            // create refresh token -> valid for 1 day
+            // send back both of them in response
+        })
+        .catch(function (err) {
+            // Send a 402 status code
+            // Send request body
+        })
+    res.json("hello");
 });
 
 app.listen(8083, () => console.log("Webservice is running on 8083"));
